db302e0131f9eeb9376f29b3c7d5ae44ca00c920
Digit4 <dpantojee775@gmail.com>
2018-12-14 19:02:58 +0530

    Added dynamic array function with partial functionality.S1 array is working corrently 2,x,y an 1,x,y>S0 does not function correctly

--- dynamic_array.py
+++ dynamic_array.py
@@ -12,4 +12,21 @@ Q = []
 for i in n:
     Q.append(list(map(int, i.split(" "))))
 
-lastAnswer = 0
+
+
+def dynamicArray(n, queries):
+	lastAnswer = 0
+	S0,S1 = [],[]
+	for query in queries:
+		if (query[0] == 1):
+			if (query[1] == 0):
+				S0.append(queries[(query[1] ^ lastAnswer) % n][-1])
+			else:
+				S1.append(queries[(query[1] ^ lastAnswer) % n][-1])
+		elif (query[0] == 2):
+			y = queries[(query[1] ^ lastAnswer) % n][-1]
+			lastAnswer = y % len(queries)
+			print(lastAnswer)
+	return [S0,S1]
+			
+print(dynamicArray(Q[0][0],Q[1:]))
