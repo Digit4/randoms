80eba04b5c402141e4d7b3804ffd680fa2ed7671
Digit4 <dpantojee775@gmail.com>
2018-12-14 20:59:09 +0530

    Added proper code for left rotation function.

--- left_rotation.py
+++ left_rotation.py
@@ -7,7 +7,27 @@ n.pop()
 Q = []
 for i in n:
     Q.append(list(map(int, i.split(" "))))
-print(Q)
+#print(Q)
+nd = inp.split()
 
-def leftRotation(arr,rot):
-	
+n = int(nd[0])
+
+d = int(nd[1])
+
+a = list(map(int, inp.rstrip().split()))
+
+print(a)
+
+
+def leftRotation(arr, rot):
+    new_array, l = [], len(arr)
+    mov_index = rot
+    for i in range(l):
+        new_array.append(arr[mov_index % l])
+        mov_index += 1
+    res = ""
+    for i in range(l):
+        res += str(new_array[i]) + " "
+    return res
+
+print(leftRotation(Q[1], 4))
